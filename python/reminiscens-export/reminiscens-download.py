c2b4f8abe7cbf0f5aa6f7ee5dfabb3a63780a327
Cristhian Parra <cdparra@gmail.com>
2013-11-09 14:44:24 +0100

    updated script with some fixes

--- python/reminiscens-export/reminiscens-download.py
+++ python/reminiscens-export/reminiscens-download.py
@@ -4,6 +4,7 @@
 # Cristhian Parra
 import urllib
 import csv
+import os
 import os.path
 import sys, argparse, csv
 import re
@@ -25,22 +26,35 @@ parser = argparse.ArgumentParser(description='download photos from URLs listed i
 parser.add_argument('file', help='csv file with URLs', action='store')
 parser.add_argument('dir', help='directory where pictures should be saved', action='store')
 args = parser.parse_args()
+
+# parse arguments
 csv_file = args.file
-pics_dir = args.dir
+picsdir = args.dir
 
 with open(csv_file, 'rb') as csvfile:
 	storyreader = csv.reader(csvfile, delimiter=',', quotechar='"')
-	included_cols = [1, 3, 4, 5]
+	included_cols = [0, 1, 3, 4, 5] # right now, using specifically structured csv for my current need, 
+								 # future version will support reading the model somewhere else 
 
 	for story in storyreader:
 		content = list(story[i] for i in included_cols)
-		print content
+		
+		storyid = content[0]	
+		title = content[1]		
+		photoid = content[2]
+		url = content[3]
+		picname = content[4]
+
 		p = re.compile('http*') # pattern to test if the url is http
-		title = content[0]		
-		photoid = content[1]
-		url = content[2]
-		pic_name = content[3]
+
 		if p.match(url):
-			print "Downloading picture ("+photoid+") "+pic_name+" of story '"+title+"'"
-			path = os.path.join(pics_dir,pic_name)
+			print "Downloading picture ("+photoid+") "+picname+" of story ("+storyid+") '"+title+"'"
+			storypath = storyid + "_" + title 
+			dirpath = os.path.join(picsdir,storypath)
+			if not os.path.exists(dirpath):
+				os.makedirs(dirpath)
+
+			path = os.path.join(dirpath,picname)
 			download_picture(url,path)
+		else: 
+			print "Story '"+title+"' has no valid URL associated"
